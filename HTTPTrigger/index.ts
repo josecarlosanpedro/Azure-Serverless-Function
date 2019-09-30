182b171c24635bdb6e1dcab1d6c184548d47bc72
unknown <josecarlosanpedro>
2019-09-30 11:23:21 +0800

    add done

--- HTTPTrigger/index.ts
+++ HTTPTrigger/index.ts
@@ -1,10 +1,11 @@
 import { AzureFunction, Context } from "@azure/functions";
-// import { startQueue, done } from "../helpers";
+import { done } from "../helpers";
 
 const ServiceBusTopicTrigger: AzureFunction = (
   context: Context,
 ) => {
   context.log('Node.js Http trigger function processed message');
+  done(context)
 };
 
 export default ServiceBusTopicTrigger;
